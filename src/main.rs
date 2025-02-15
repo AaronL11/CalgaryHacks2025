e7ca747140790e81380238eb21c3777d7bd3ec70
Aaron Lengyel <aaron_lengyel@ucalgary.ca>
2025-02-15 16:09:30 -0700

    More or less ready to go

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{prelude::*,BufReader};
+use std::net::{TcpListener,SocketAddrV4,Ipv4Addr,TcpStream};
+
+
+fn main() -> std::io::Result<()> {
+    let addr = SocketAddrV4::new(Ipv4Addr::new(127,0,0,0),8000);
+    let listener = TcpListener::bind(addr)?;
+
+    let (stream,strm_addr) = listener.accept()?;
+    handle(stream);
+
+    Ok(())
+}
+
+fn handle(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
 }
