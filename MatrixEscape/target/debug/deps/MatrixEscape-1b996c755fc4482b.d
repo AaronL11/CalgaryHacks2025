f7b6914924890651ee0e8dfe975d944b62459128
Aaron Lengyel <aaron_lengyel@ucalgary.ca>
2025-02-15 15:33:01 -0700

    Setting up the project

---
+++ MatrixEscape/target/debug/deps/MatrixEscape-1b996c755fc4482b.d
@@ -0,0 +1,5 @@
+/Users/aaron_lengyel/Documents/Repos/CalgaryHacks2025/MatrixEscape/target/debug/deps/libMatrixEscape-1b996c755fc4482b.rmeta: src/main.rs
+
+/Users/aaron_lengyel/Documents/Repos/CalgaryHacks2025/MatrixEscape/target/debug/deps/MatrixEscape-1b996c755fc4482b.d: src/main.rs
+
+src/main.rs:
