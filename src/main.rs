b3293839fc46a74d097740d18280649f60bae336
Aaron Lengyel <aaron_lengyel@ucalgary.ca>
2025-02-16 01:43:21 -0700

    server working

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,28 @@ use std::net::{TcpListener,SocketAddrV4,Ipv4Addr,TcpStream};
 
 
 fn main() -> std::io::Result<()> {
-    let addr = SocketAddrV4::new(Ipv4Addr::new(127,0,0,0),8000);
+    let addr = SocketAddrV4::new(Ipv4Addr::new(10,13,68,31),8000);
+    println!("Booting up server");
     let listener = TcpListener::bind(addr)?;
 
-    let (stream,strm_addr) = listener.accept()?;
-    handle(stream);
-
+    println!("listening on: {addr}");
+    loop {
+        let (stream,strm_addr) = listener.accept()?;
+        println!("connected to address: {strm_addr}");
+        handle(stream)?;
+        println!("Connection closed with {addr}\n");
+    }
     Ok(())
 }
 
-fn handle(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&stream);
+fn handle(mut stream: TcpStream) -> std::io::Result<()> {
+    let mut buf_reader = BufReader::new(&stream);
+    let mut buf = vec![0;10];
+
+    buf_reader.read(&mut  buf[..])?;
+    println!("Message received: {}", String::from_utf8(buf).unwrap());
+    let msg = "hai";
+    stream.write_all(msg.as_bytes())?;
+    println!("Message sent: {msg}");
+    Ok(())
 }
